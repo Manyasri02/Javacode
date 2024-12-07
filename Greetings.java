61ee0aedc1c429a83fb455ce442239e7bc2c45a6
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2024-12-07 08:05:05 +0530

    Added some more greeting buttons

--- Greetings.java
+++ Greetings.java
@@ -19,6 +19,14 @@ public class Greetings
         case 3:
             System.out.println("Bonjour");
             break;
+        case 4:
+            System.out.println("Ciao")
+            break;
+        case 5:
+            System.out.println("Hola")
+            break;
+        case 6:
+            
         default:
             System.out.println("Invalid Button");
         }
