5ad7c58291528b2497fefe22bfb5626dc2f6c8db
Manyasri02 <118347380+Manyasri02@users.noreply.github.com>
2024-12-13 21:49:33 +0530

    Add files via upload

---
+++ Checksorted.java
@@ -0,0 +1,25 @@
+//check if an array is sorted(Strictly Increasing)
+public class Checksorted
+{
+    public static boolean isSorted(int arr[], int idx)
+    {
+        if(idx == arr.length-1)
+        {
+            return true;
+        }
+        if(arr[idx] < arr[idx+1])
+        {
+            //array is sorted till now
+            return isSorted(arr, idx+1);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String args[])
+    {
+        int arr[] = {1,2,3};
+        System.out.println(isSorted(arr, 0));
+    }
+}
