81233af5f14cda046892bb9756f77c4cecc61376
Manyasri02 <118347380+Manyasri02@users.noreply.github.com>
2024-12-16 21:08:37 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Substring
+{
+    public static void main(String args [])
+    {
+        String sentence ="TonyStark";
+        String name= sentence.substring(0,4);
+        String name1= sentence.substring(4);
+        System.out.println(name);
+        System.out.println(name1);
+    }
+}
