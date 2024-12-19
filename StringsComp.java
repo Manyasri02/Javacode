3ee670bba5abe33d7c867b0be95e4a71d28acd5e
Manyasri02 <118347380+Manyasri02@users.noreply.github.com>
2024-12-19 23:12:43 +0530

    Add files via upload

---
+++ StringsComp.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class StringsComp
+{
+    public static void main(String args[])
+    {
+        //compare
+        String name1="Tony";
+        String name2="Tony2";
+
+        //1 s1 > s2 : +ve value
+        //2 s1 == s2 : 0
+        //3 s1 < s2 : -ve value
+        //hello cello
+
+        if(name1.compareTo(name2) == 0)
+        {
+            System.out.println("Strings are equal");
+        }
+        else
+        {
+            System.out.println("Strings are not equal");
+        }
+
+    }
+}
