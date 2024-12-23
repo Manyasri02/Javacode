03a04d3b26f3065f4805be6534ad77c1f1735cbd
Manyasri02 <118347380+Manyasri02@users.noreply.github.com>
2024-12-23 23:31:24 +0530

    Add files via upload

---
+++ Setbit.java
@@ -0,0 +1,13 @@
+impo  rt java.util.*;
+public class Setbit
+{
+    public static void main(String args [])
+    {
+        int n=5;
+        int pos =1;
+        int bitMask =1<<pos;
+
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+    }
+}
